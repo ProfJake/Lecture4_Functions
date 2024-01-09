b52f84c6611ffb170735c29c3213f3fd24d93123
Jake Levy <jake.r.levy@gmail.com>
2024-01-09 00:03:51 -0500

    Added a new file

---
+++ simpleFunctions.js
@@ -0,0 +1,14 @@
+// A file demoing simple functions in JS
+// Prof Jake Levy
+// APW: JavaScript
+function add2Numbers(num1, num2){
+    return num1 + num2
+}
+
+function greeting(name){
+    console.log(`Hello ${name}`)
+}
+
+console.log(add2Numbers(5, 3))
+console.log(add2Numbers("five", 3))
+console.log(greeting("Jake"))
